9ab15a75f9281d97c71ea44e2b1fd63746f4d04f
mdiop23 <madina.diop20@gmail.com>
2023-10-27 20:04:54 -0400

    added new file structure

---
+++ MathScripts/taxes.js
@@ -0,0 +1,15 @@
+"use strict";
+
+// inputs
+var salary
+var fedTaxRate 
+salary = 60000
+fedTaxRate = .23
+
+//calculations
+var fedTaxWithheld = salary * fedTaxRate
+
+console.log(fedTaxWithheld)
+
+fedTaxWithheld = parseFloat(fedTaxWithheld.toFixed(2))
+
